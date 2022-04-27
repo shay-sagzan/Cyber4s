0c0c0bb8062d6ee83b8f8c46c828ac364351dc76
Shay Sagzan <sagzanster@gmail.com>
2022-04-27 18:32:05 +0300

    Fixed Bugs

--- script/app.js
+++ script/app.js
@@ -1,14 +1,10 @@
 import { BoardData } from "./BoardData.js"
 import { King, Knight, Pawn, Piece, Rook, Bishop, Queen } from "./Piece.js"
 import { GameManager } from "./GameManager.js"
-import { Utilities } from "./Utilities.js"
 
 const newMoves = new Piece()
-const util = new Utilities()
 const game = new GameManager()
 export const boardData = new BoardData([
   ...game.blackPieces,
   ...game.whitePieces,
 ])
-
-// window.addEventListener("load", boardData.initGame())
